315a09f29e2b36593f80852e1d8d8baa86609155
Gervis Bermudez <gervisbermudez15@gmail.com>
2021-02-08 21:32:05 -0300

    use useState

---
+++ src/components/CounterApp.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export const CounterApp = () => {
+
+    const [state, setState] = useState({
+        counter1: 10,
+        counter2: 20,
+    });
+
+    const { counter1, counter2 } = state;
+
+    return (
+        <div className="container">
+            <h1>Counter {counter1}</h1>
+            <h1>Counter {counter2}</h1>
+            <button className="btn btn-primary"
+                onClick={() => {
+                    setState({
+                        ...state,
+                        counter1: counter1 + 1
+                    });
+                }}
+            >+1</button>
+        </div>
+    )
+}   
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CounterApp } from './components/CounterApp';
 import { HookApp } from './HookApp';
 
 ReactDOM.render(
-  <HookApp />,
+  <CounterApp />,
   document.getElementById('root')
 );
 
